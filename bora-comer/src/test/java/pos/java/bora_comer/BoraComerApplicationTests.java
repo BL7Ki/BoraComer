15c5db044460f4a8a477c427b87a5d38ecf7836e
Leonardo Ferreira <leoventura245@gmail.com>
2025-04-18 18:10:40 -0300

    iniciando o projeto

---
+++ bora-comer/src/test/java/pos/java/bora_comer/BoraComerApplicationTests.java
@@ -0,0 +1,13 @@
+package pos.java.bora_comer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoraComerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
