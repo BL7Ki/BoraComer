b0284894e8a02f3a0eb52b59d1ee2f2c1cede2b5
evertoncsteixeira <evertoncristiano86@uol.com.br>
2025-05-10 14:39:09 -0300

    teste commit

---
+++ bora-comer/src/main/java/pos/java/bora_comer/dto/UsuarioResquestDTO.java
@@ -0,0 +1,14 @@
+package pos.java.bora_comer.dto;
+
+public record UsuarioResquestDTO(
+        String nome,
+        String email,
+        String login,
+        String senha,
+        String telefone,
+        String dataUltAlteracao,
+        String endereco 
+  ) {
+
+  }
+ 
