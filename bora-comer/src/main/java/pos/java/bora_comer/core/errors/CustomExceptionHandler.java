148a41cc8de4a3b319889db25c68319ea5885424
wagnersistemalima <wagner.sistemalima@gmail.com>
2025-05-12 14:50:58 -0300

    criacao do error handler que vai tratar excecoes na aplicacao

---
+++ bora-comer/src/main/java/pos/java/bora_comer/core/errors/CustomExceptionHandler.java
@@ -0,0 +1,49 @@
+package pos.java.bora_comer.core.errors;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class CustomExceptionHandler {
+
+    @ExceptionHandler(SummerException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErroView SummerException(SummerException exception, HttpServletRequest request) {
+        return new ErroView(
+                LocalDateTime.now(),
+                HttpStatus.BAD_REQUEST.value(),
+                HttpStatus.BAD_REQUEST.name(),
+                exception.getMessage(),
+                request.getServletPath()
+        );
+    }
+
+    @ExceptionHandler(UserDomainException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErroView SummerParseException(UserDomainException exception, HttpServletRequest request) {
+        return new ErroView(
+                LocalDateTime.now(),
+                HttpStatus.BAD_REQUEST.value(),
+                HttpStatus.BAD_REQUEST.name(),
+                exception.getMessage(),
+                request.getServletPath()
+        );
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErroView handleGenericException(Exception exception, HttpServletRequest request) {
+        return new ErroView(
+                LocalDateTime.now(),
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                HttpStatus.INTERNAL_SERVER_ERROR.name(),
+                exception.getMessage(),
+                request.getServletPath()
+        );
+    }
+}
