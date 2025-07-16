1be653eaf75ef6604dc02c943edb54a56bc4f794
Leonardo Ferreira <leoventura245@gmail.com>
2025-07-15 23:28:22 -0300

    consertando os testes que quebraram com UserType

--- src/test/java/pos/java/bora_comer/util/UserTestFactory.java
+++ src/test/java/pos/java/bora_comer/util/UserTestFactory.java
@@ -1,24 +1,21 @@
 package pos.java.bora_comer.util;
 
-import pos.java.bora_comer.core.domain.Address;
 import pos.java.bora_comer.core.domain.user.User;
 import pos.java.bora_comer.core.domain.user.UserRoleEnum;
+import pos.java.bora_comer.core.domain.userType.UserTypeNameEnum;
 import pos.java.bora_comer.infra.persistence.repository.user.entity.AddressEntity;
 import pos.java.bora_comer.infra.persistence.repository.user.entity.UserEntity;
 import pos.java.bora_comer.infra.persistence.repository.user.entity.UserRoleEntityEnum;
 
 public class UserTestFactory {
 
+    private static final Long USER_TYPE_ID_DEFAULT = 1L;
+
     public static User umUserComId(Long id) {
         return User.create(
-                id,
-                "Messi",
-                "messi@ex.com",
-                "messi",
-                "Messi@123",
-                Address.create("Rua A", "Bairro B", "Cidade C", "SP", "12345-678"),
-                UserRoleEnum.CLIENTE,
-                "2024-06-25"
+                id, "Messi", "messi@ex.com", "messi", "Messi@123",
+                AddressTestFactory.umEnderecoPadrao(), UserRoleEnum.DEFAULT,
+                "2024-06-25", "2024-06-25", UserTypeNameEnum.CLIENTE
         );
     }
 
@@ -28,80 +25,34 @@ public class UserTestFactory {
     }
 
     public static User umUserPadrao() {
-        return User.create(
-                1L,
-                "Messi",
-                "messi@ex.com",
-                "messi",
-                "Messi@123",
-                Address.create("Rua A", "Bairro B", "Cidade C", "SP", "12345-678"),
-                UserRoleEnum.CLIENTE,
-                "2024-06-25"
-        );
+        return umUserComId(1L);
     }
 
     public static User umUserAtualizado(Long id) {
         return User.create(
-                id,
-                "Messi Atualizado",
-                "messi_novo@ex.com",
-                "messi",
-                "NovaSenha@123",
-                Address.create("Rua Nova", "Bairro Novo", "Cidade X", "SP", "98765-432"),
-                UserRoleEnum.ADMIN,
-                "2024-06-26"
-        );
-    }
-
-    public static User umUserPadraoCliente() {
-        return User.create(
-                1L,
-                "Messi",
-                "messi@ex.com",
-                "messi",
-                "Messi@123",
-                Address.create("Rua A", "Bairro B", "Cidade C", "SP", "12345-678"),
-                UserRoleEnum.CLIENTE,
-                "2024-06-25"
+                id, "Messi Atualizado", "messi_novo@ex.com", "messi", "NovaSenha@123",
+                AddressTestFactory.umEnderecoAtualizado(), UserRoleEnum.DEFAULT,
+                "2024-06-26", "2024-06-26", UserTypeNameEnum.CLIENTE
         );
     }
 
     public static UserEntity umUserEntityPadrao() {
         return UserEntity.create(
-                "Messi",
-                "messi@ex.com",
-                "messi",
-                "Messi@123",
-                AddressEntity.create("Rua A", "Bairro B", "Cidade C", "SP", "12345-678"),
-                UserRoleEntityEnum.CLIENTE
-        );
-    }
-
-    public static UserEntity umUserEntityPadraoCliente() {
-        return UserEntity.create(
-                "Messi",
-                "messi@ex.com",
-                "messi",
-                "Messi@123",
-                AddressEntity.create("Rua A", "Bairro B", "Cidade C", "SP", "12345-678"),
-                UserRoleEntityEnum.CLIENTE
+                "Messi", "messi@ex.com", "messi", "Messi@123",
+                AddressTestFactory.umEnderecoEntityPadrao(),
+                UserRoleEntityEnum.DEFAULT, USER_TYPE_ID_DEFAULT
         );
     }
 
     public static UserEntity umUserEntityComDadosDe(User user) {
         return UserEntity.create(
-                user.getName(),
-                user.getEmail(),
-                user.getUsername(),
-                user.getPassword(),
+                user.getName(), user.getEmail(), user.getUsername(), user.getPassword(),
                 AddressEntity.create(
-                        user.getAddress().getStreet(),
-                        user.getAddress().getCity(),
-                        user.getAddress().getState(),
-                        user.getAddress().getZipCode(),
+                        user.getAddress().getStreet(), user.getAddress().getCity(),
+                        user.getAddress().getState(), user.getAddress().getZipCode(),
                         user.getAddress().getZipCode()
                 ),
-                UserRoleEntityEnum.valueOf(user.getUserRoleEnum().name())
+                UserRoleEntityEnum.valueOf(user.getUserRoleEnum().name()), USER_TYPE_ID_DEFAULT
         );
     }
 }
