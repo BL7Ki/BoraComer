d865c5ca14eac29a7c8ec4e1c64ef18da79a3fbd
Leonardo Ferreira <leoventura245@gmail.com>
2025-07-26 13:45:27 -0300

    atualizando o script sql, criando a entidade e a repository do cardapio, além da interface do gateway para depois poder implementar os metodos

---
+++ src/main/java/pos/java/bora_comer/infra/persistence/repository/menu/MenuItemRepository.java
@@ -0,0 +1,14 @@
+package pos.java.bora_comer.infra.persistence.repository.menu;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pos.java.bora_comer.infra.persistence.repository.menu.entity.MenuItemEntity;
+
+import java.util.List;
+
+@Repository
+public interface MenuItemRepository extends JpaRepository<MenuItemEntity, Long> {
+    List<MenuItemEntity> findByInPlaceOnlyTrue();
+    List<MenuItemEntity> findByNameContainingIgnoreCase(String name);
+
+}
