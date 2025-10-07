cece0ff1d59bc7ec68aaaab95ab86f91a3c3bd36
Leonardo Ferreira <leoventura245@gmail.com>
2025-10-07 19:47:22 -0300

    criando o schema graphql e refatorando o Restaurant para fazer sentido

--- src/main/java/pos/java/bora_comer/infra/gateway/restaurant/impl/RestaurantSearchGatewayImpl.java
+++ src/main/java/pos/java/bora_comer/infra/gateway/restaurant/impl/RestaurantSearchGatewayImpl.java
@@ -2,12 +2,15 @@ package pos.java.bora_comer.infra.gateway.restaurant.impl;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import pos.java.bora_comer.core.domain.restaurant.Restaurant;
 import pos.java.bora_comer.core.errors.SummerNotFoundException;
 import pos.java.bora_comer.core.gateway.restaurant.RestaurantSearchGateway;
 import pos.java.bora_comer.core.mapper.restaurant.RestaurantMapper;
 import pos.java.bora_comer.infra.persistence.repository.restaurant.RestaurantRepository;
+import pos.java.bora_comer.infra.persistence.repository.restaurant.entity.RestaurantEntity;
 
 @Component
 public class RestaurantSearchGatewayImpl implements RestaurantSearchGateway {
@@ -28,8 +31,16 @@ public class RestaurantSearchGatewayImpl implements RestaurantSearchGateway {
     }
 
     @Override
-    public Page<Restaurant> findAll(int page, int size) {
-        return restaurantRepository.findAll(PageRequest.of(page, size))
-                .map(restaurantMapper::toDomain);
+    public Page<Restaurant> findAll(int page, int size, String cuisineType) {
+        Pageable pageable = PageRequest.of(page, size);
+        Page<RestaurantEntity> resultPage;
+
+        if (StringUtils.hasText(cuisineType)) {
+            resultPage = restaurantRepository.findByCuisineType(cuisineType, pageable);
+        } else {
+            resultPage = restaurantRepository.findAll(pageable);
+        }
+
+        return resultPage.map(restaurantMapper::toDomain);
     }
 }
